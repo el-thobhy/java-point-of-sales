6cd1bef32ed7db8f7b6d8c4db78d651ae8d54d53
el-thobhy <auriwan.yasper@sci.ui.ac.id>
2024-05-16 08:49:43 +0700

    membuat tampilan modal pop up

--- src/main/java/com/elthobhy/javapos/controllers/CategoryController.java
+++ src/main/java/com/elthobhy/javapos/controllers/CategoryController.java
@@ -1,21 +1,57 @@
 package com.elthobhy.javapos.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.elthobhy.javapos.models.Category;
 
 @Controller
 @RequestMapping("/category")
 public class CategoryController {
+    
     @GetMapping("")
     ModelAndView Index(){
-        return new ModelAndView("/category/index");
+        List<Category> data = new ArrayList<Category>();
+        // fill data
+        data.add(new Category());
+        data.get(data.size()-1).setId(10);
+        data.get(data.size()-1).setName("Makanan");
+        data.get(data.size()-1).setDescription("Makanan 2");
+        data.add(new Category());
+        data.get(data.size()-1).setId(11);
+        data.get(data.size()-1).setName("Minuman");
+        data.get(data.size()-1).setDescription("Minuman 2");
+
+        //add data to view
+        ModelAndView view = new ModelAndView("/category/index");
+        view.addObject("data", data);
+        return view;
     }
 
     @GetMapping("/{id}")
     ModelAndView Detail(@PathVariable int id){
-        return new ModelAndView("/category/detail");
+        ModelAndView view = new ModelAndView("/category/detail");
+        view.addObject("id", id);
+        return view;
+    }
+    @GetMapping("/add")
+    ModelAndView Add(){
+        ModelAndView view = new ModelAndView("/category/add");
+        Category cat = new Category();
+        view.addObject("category", cat);
+        return view;
+    }
+
+    @PostMapping("/save")
+    ModelAndView Save(@ModelAttribute Category data){
+        return new ModelAndView("redirect:/category");
     }
 }
--- src/main/java/com/elthobhy/javapos/models/Category.java
+++ src/main/java/com/elthobhy/javapos/models/Category.java
@@ -1,8 +1,91 @@
 package com.elthobhy.javapos.models;
 
+import java.time.LocalDateTime;
+
 // import jakarta.persistence.Entity;
 
 // @Entity
-// public class Category {
-    
-// }
+public class Category {
+    private long Id;
+    private String Name;
+    private String Description;
+    private boolean IsDeleted;
+    private int CreateBy;
+    private LocalDateTime CraeteDate;
+    private int UpdateBy;
+    private LocalDateTime UpdateDate;
+    public long getId() {
+        return Id;
+    }
+    public void setId(long id) {
+        Id = id;
+    }
+    public String getName() {
+        return Name;
+    }
+    public void setName(String name) {
+        Name = name;
+    }
+    public String getDescription() {
+        return Description;
+    }
+    public void setDescription(String description) {
+        Description = description;
+    }
+    public boolean isIsDeleted() {
+        return IsDeleted;
+    }
+    public void setIsDeleted(boolean isDeleted) {
+        IsDeleted = isDeleted;
+    }
+    public int getCreateBy() {
+        return CreateBy;
+    }
+    public void setCreateBy(int createBy) {
+        CreateBy = createBy;
+    }
+    public LocalDateTime getCraeteDate() {
+        return CraeteDate;
+    }
+    public void setCraeteDate(LocalDateTime craeteDate) {
+        CraeteDate = craeteDate;
+    }
+    public int getUpdateBy() {
+        return UpdateBy;
+    }
+    public void setUpdateBy(int updateBy) {
+        UpdateBy = updateBy;
+    }
+    public LocalDateTime getUpdateDate() {
+        return UpdateDate;
+    }
+    public void setUpdateDate(LocalDateTime updateDate) {
+        UpdateDate = updateDate;
+    }
+
+    // public Category(long id, String name, String description, boolean isDeleted, int createBy, LocalDateTime craeteDate,
+    //         int updateBy, LocalDateTime updateDate) {
+    //     Id = id;
+    //     Name = name;
+    //     Description = description;
+    //     IsDeleted = isDeleted;
+    //     CreateBy = createBy;
+    //     CraeteDate = craeteDate;
+    //     UpdateBy = updateBy;
+    //     UpdateDate = updateDate;
+    // }
+    // public Category(long id, String name, String description){
+    //     this(
+    //         id, 
+    //         name, 
+    //         description ,
+    //         false, 
+    //         1 ,
+    //         LocalDateTime.now(),
+    //         1, 
+    //         LocalDateTime.now());
+    // }
+    // public Category(){
+    //     this(0, "", "", false, 0, null, 0, null);
+    // }
+}
