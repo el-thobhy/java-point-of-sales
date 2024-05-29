999c91de4ae6dcfded63ea0d4aac6cf38cff8ca4
el-thobhy <auriwan.yasper@sci.ui.ac.id>
2024-05-29 09:46:33 +0700

    Menambahkan validasi pada category input dan product

--- src/main/java/com/elthobhy/javapos/controllers/VariantController.java
+++ src/main/java/com/elthobhy/javapos/controllers/VariantController.java
@@ -1,13 +1,13 @@
 package com.elthobhy.javapos.controllers;
 
-import java.util.List;
-import java.util.Map;
+// import java.util.List;
+// import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
+// import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.RequestEntity;
+// import org.springframework.http.MediaType;
+// import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
